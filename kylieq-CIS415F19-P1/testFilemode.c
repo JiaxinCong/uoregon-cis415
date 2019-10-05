1a9693d1faf5f778835edc1155b0106ba692e570
Kylie <kylieq@uoregon.edu>
2019-10-05 16:42:02 -0700

    Add changes to file mode

--- kylieq-CIS415F19-P1/testFilemode.c
+++ kylieq-CIS415F19-P1/testFilemode.c
@@ -1,14 +1,19 @@
-int main(int argc, char argv[]) {
-	char file_name[] = argv[1];
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	char file_name[] = "input.txt";
 	FILE *fp = fopen(file_name, "r");
 	if (fp == NULL) {
 		printf("Error: File not found: %s\n", file_name);
 		exit(1);
 	}
 
-	char *buffer;
-	size_t bufsize = 100;
-	size_t num_char;
+	char *buffer = NULL;
+	size_t bufsize = NULL;
+	size_t num_char = 0;
 
 	const char s[3]= " \n";
 	char *token;
@@ -18,19 +23,19 @@ int main(int argc, char argv[]) {
 		int ctr = 0;
 		ptr = (char**)malloc(20*sizeof(char*));
 
-		printf("\n%s\n", buffer);
+//		printf("\n%s\n", buffer);
 
 		token = strtok(buffer, s);
 		ptr[0] = token;
 		ctr = 1;
 
-        while (token != NULL) {
-        	checkCommand(token);
-           	token = strtok(NULL, s);
-          	ptr[ctr++] = token;
-        }
-        ptr[ctr-1] = "NULL";
-        splitFile(ptr);
+	        while (token != NULL) {
+        	   	token = strtok(NULL, s);
+          		ptr[ctr++] = token;
+        	}
+
+        	ptr[ctr-1] = "NULL";
+//        	splitFile(ptr);
 	}
 
 	/*Free the allocated memory*/
