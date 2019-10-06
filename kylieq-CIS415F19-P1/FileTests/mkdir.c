3492b0bb308e5bb100c506f565ce5fea62bf7b7b
Kylie <kylieq@uoregon.edu>
2019-10-06 06:04:31 -0700

    add changes

--- kylieq-CIS415F19-P1/FileTests/mkdir.c
+++ kylieq-CIS415F19-P1/FileTests/mkdir.c
@@ -5,8 +5,7 @@ int main(int argc, char *argv[]) { /*for the mkdir command*/
 	char *dirName = argv[1];
 	struct stat st = {0};
 	if (stat(dirName, &st) == -1) {
-//		mkdir(dirName, 0700);
-		mkdir(dirName);
+		mkdir(dirName, 1000);
 	}
 }
 
