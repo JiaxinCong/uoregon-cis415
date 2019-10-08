1e942997eee5fdfe9466005f72338cd46a45db39
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-07 22:34:23 -0700

    Add changes made to lab2-b.c

---
+++ Lab2/lab2-b.c
@@ -0,0 +1,86 @@
+/*
+* Description: The function opens the file input.txt for reading only. A 
+* buffer is allocated to hold the contents of said file. This new string is 
+* broken up into a series of tokens using white space as the delimiter, and 
+* then displayed line by line. Once the end of the file has been reached, the 
+* user may either enter text to be split into tokens, or end the program.
+*
+* Author: Kylie Quan
+*
+* Date: 7 October 2019
+*
+* Notes: 
+* 1. After the end of the file has been reached, user input is required before 
+*    the program may proceed. Enter a line of text for the program to then break 
+*    it up into a series of tokens. Otherwise, type "exit" to terminate the 
+*    program.
+*/
+
+/*-------------------------Preprocessor Directives---------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*---------------------------------------------------------------------------*/
+
+/*-----------------------------Program Main----------------------------------*/
+int main() {
+	setbuf(stdout, NULL);
+	
+	/* Main Function Variables */
+	FILE *fp = fopen("input.txt", "r");
+
+	char *buffer;
+	size_t bufsize = 100;
+	size_t num_char;
+
+	const char s[2]= " \n";
+	char *token;
+	
+	char exitStr[100];
+	strcpy(exitStr, "exit");
+
+	/* Allocate memory for the input buffer. */
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	
+	/*main run loop*/
+	while(1) {
+		/* Print >>> then get the input string */
+		num_char = getline(&buffer, &bufsize, fp);
+		printf(">>> %s\n", buffer);
+
+		/* Tokenize the input string */
+		token = strtok(buffer, s);
+
+		/* Display each token */
+		int ctr = 0;
+		while (token != NULL) {
+			printf("T%d: %s\n", ctr, token);
+			token = strtok(NULL, s);
+			ctr++;
+		}
+		
+		/* If the user entered <exit> then exit the loop */
+		printf(">>> ");
+		num_char = getline(&buffer, &bufsize, stdin);
+		token = strtok(buffer, s);
+
+        /* Check user input */
+		if (strcmp(exitStr, buffer) == 0) {
+			break;
+		}
+		else {
+			/* Display each token */
+			int ctr = 0;
+			while (token != NULL) {
+				printf("T%d: %s\n", ctr, token);
+				token = strtok(NULL, s);
+				ctr++;
+			}
+		}
+	}
+
+	/*Free the allocated memory*/
+	free(buffer);
+	return 1;
+}
+/*-----------------------------Program End-----------------------------------*/
