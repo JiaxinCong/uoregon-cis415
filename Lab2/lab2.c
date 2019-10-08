0c66d3c2a4f8ad0be5d0868c37bcd3aeee16bc7f
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-07 19:09:27 -0700

    Add lab2.c

---
+++ Lab2/lab2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+int input(char *s, int length);
+
+int main(int args, char *argv[])  {
+	char *buffer;
+	size_t bufsize = 100;
+	size_t num_char;
+
+	const char s[2]= " ";
+	char *token;
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	num_char = getline(&buffer, &bufsize, stdin);
+
+	// Get the first token.
+	token = strtok(buffer, s);
+
+	// Walk through other tokens.
+	int ctr = 0;
+	while (token != NULL) {
+		printf("T%d: %s\n", ctr, token);
+		token = strtok(NULL, s);
+		ctr++;
+	}
+
+	return(0);
+
+}
