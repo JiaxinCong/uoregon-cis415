6c38e06bce5ad5042b3eb853162b24cc77196ec4
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-16 11:33:24 -0700

    Add test for split file

--- kylieq-CIS415F19-P1/testSplitF.c
+++ kylieq-CIS415F19-P1/testSplitF.c
@@ -27,3 +27,44 @@ int splitFile(char** arr) {
 	free(ptr);
 	return 1;
 }
+
+int main() {
+	char file_name[] = "input.txt";
+	FILE *fp = fopen(file_name, "r");
+	if (fp == NULL) {
+		printf("Error: File not found: %s\n", file_name);
+		exit(1);
+	}
+
+	char *buffer = NULL;
+	size_t bufsize = NULL;
+	size_t num_char = 0;
+
+	const char s[3]= " \n";
+	char *token;
+
+	char **ptr;
+	while ((num_char = getline(&buffer, &bufsize, fp)) != -1) {
+		int ctr = 0;
+		ptr = (char**)malloc(20*sizeof(char*));
+
+//		printf("\n%s\n", buffer);
+
+		token = strtok(buffer, s);
+		ptr[0] = token;
+		ctr = 1;
+
+	        while (token != NULL) {
+        	   	token = strtok(NULL, s);
+          		ptr[ctr++] = token;
+        	}
+
+        	ptr[ctr-1] = "NULL";
+        	splitFile(ptr);
+	}
+
+	/*Free the allocated memory*/
+	free(ptr);
+	fclose(fp);
+	return 1;
+}
