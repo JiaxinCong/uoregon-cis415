f41d77d667085d5dd58ae3ee9727e286cc4d05fb
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-16 11:46:00 -0700

    Add test for split line

--- kylieq-CIS415F19-P1/testSplitLine.c
+++ kylieq-CIS415F19-P1/testSplitLine.c
@@ -3,6 +3,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+int checkCommand(char *token) {
+	// If command requires 0 args, return 1
+	// If command requires 1 arg, return 2
+	// If commmand requires 2 args, return 3
+	// If command not valid, return 0
+
+    if (strcmp(token, "ls") == 0 || strcmp(token, "ls") == 13) { // 0 args
+    	return 1;
+   	}
+    else if (strcmp(token, "pwd") == 0 || strcmp(token, "pwd") == 13) { // 0 args
+    	return 1;
+    }
+    else if (strcmp(token, "mkdir") == 0 || strcmp(token, "mkdir") == 13) { // 1 arg
+    	return 2;
+    }
+    else if (strcmp(token, "cd") == 0 || strcmp(token, "cd") == 13) { // 1 arg
+    	return 2;
+    }
+    else if (strcmp(token, "cp") == 0 || strcmp(token, "cp") == 13) { // 2 args
+    	return 3;
+    }
+    else if (strcmp(token, "mv") == 0 || strcmp(token, "mv") == 13) { // 2 args
+    	return 3;
+    }
+    else if (strcmp(token, "rm") == 0 || strcmp(token, "rm") == 13) { // 1 arg
+    	return 2;
+    }
+    else if (strcmp(token, "cat") == 0 || strcmp(token, "cat") == 13) { // 1 arg
+    	return 2;
+    }
+    else {
+        return 0; 
+    }
+}
+
 int splitLine(char** arr) {
 	int ctr = 0;
 	/* Get total count of valid entries */
