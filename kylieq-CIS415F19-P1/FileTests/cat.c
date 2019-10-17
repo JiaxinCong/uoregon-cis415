7e091a95d9ab2e52a72fd6eb470b09f8814b81e8
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-17 09:28:17 -0700

    add test for cat

---
+++ kylieq-CIS415F19-P1/FileTests/cat.c
@@ -0,0 +1,35 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void displayFile(char *filename) { /*for the cat command*/
+	char newFn[strlen(filename)-1];
+	int i;
+	for (i=0; i<strlen(filename)-1; i++) {
+		newFn[i] = filename[i];
+	}
+	newFn[i] = '\0';
+
+	FILE *fp = fopen(newFn, "r");
+
+	if (fp == NULL) {
+		printf("Error: File not found: %s\n", filename);
+	}
+	else {
+		char *buffer;
+		size_t bufsize = 100;
+		size_t num_char;
+
+		while ((num_char = getline(&buffer, &bufsize, fp)) != -1) {
+			printf("%s", buffer);			
+		}
+	}
+
+	fclose(fp);
+}
