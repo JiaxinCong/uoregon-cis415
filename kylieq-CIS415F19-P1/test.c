0deeb4d6fb84573fb533940aae361febdd49b0a8
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-17 15:30:32 -0700

    add changes

--- kylieq-CIS415F19-P1/test.c
+++ kylieq-CIS415F19-P1/test.c
@@ -3,84 +3,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-int splitFile(char** arr, int mode) {
-	int i = 0;
-	char** ptr = NULL;
-	while (i < sizeof(arr)) {
-		int ctr = 0;
-		ptr = (char**)malloc(10*sizeof(char*));
+int main() {
+	char *file_name = "input.txt";
+	FILE *fp = fopen(file_name, "r");
 
-		while ((strcmp(arr[i], ";") != 0) && (strcmp(arr[i], "NULL") != 0)) {
-			ptr[ctr] = arr[i];
-			ctr++;
-			i++;
-		}
-
-//		splitLine(ptr, mode);
-
-		if (strcmp(arr[i], "NULL") == 0) {
-			break;
-		}
-
-		i++;
-	}
-	free(ptr);
-	return 1;
-}
-
-int main() {	
-	/* Main Function Variables */
-	char *buffer = NULL;
-	size_t bufsize = 100;
-
-	const char *s = " \n";
-	const char *exitStr = "exit";
-	char *token = NULL;
-
-	/* Allocate memory for the input buffer. */
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if (buffer == NULL) {
-		printf("Error: Unable to allocate input buffer.\n");
+	if (fp == NULL) {
+		printf("Error: File not found: %s\n", file_name);
 		exit(1);
 	}
-	
-	char **ptr = NULL;
-	while(1) {
+
+	char *buffer = NULL;
+	size_t bufsize = NULL;
+	size_t num_char = 0;
+
+	const char *s = " \n";
+	char *token;
+
+	char **ptr;
+	while ((num_char = getline(&buffer, &bufsize, fp)) != -1) {
 		int ctr = 0;
-		ptr = (char**)malloc(20 * sizeof(char*));
+		ptr = (char**)malloc(20*sizeof(char*));
 
-		/* Print >>> then get the input string */
-		printf(">>> ");
-		getline(&buffer, &bufsize, stdin);
+		printf("\n%s\n", buffer);
 
-		/* Tokenize the input string */
 		token = strtok(buffer, s);
+
 		ptr[0] = token;
 		ctr = 1;
 
-		while (token != NULL) {
-			if (strcmp(exitStr, buffer) == 0) {
-				break;
-			}
+        while (token != NULL) {
+           	token = strtok(NULL, s);
+          	ptr[ctr++] = token;
+        }
 
-           		token = strtok(NULL, s);
-          		ptr[ctr++] = token;
-		}
-
-		ptr[ctr-1] = "NULL";
-
-		/* Check if user entered anything on the command line at all */
-		if (strcmp(ptr[0], "NULL") != 0) {
-	        	splitFile(ptr, -2);
-        	}
-
-		if (token != NULL) {
-			if (strcmp(exitStr, buffer) == 0) {
-				break;
-			}
-		}
-		free(ptr);
+        ptr[ctr-1] = "NULL";
+//        splitFile(ptr, -1);
 	}
-	free(buffer);
-	exit(0);
+
+	/*Free the allocated memory*/
+	free(ptr);
+	fclose(fp);
+	return 1;
 }
