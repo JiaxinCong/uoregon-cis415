68a91da88bb8068cc8acedeabc9fd719c3e722cc
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-16 19:32:42 -0700

    Add changes

--- kylieq-CIS415F19-P1/FileTests/testintermode.c
+++ kylieq-CIS415F19-P1/FileTests/testintermode.c
@@ -3,6 +3,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+int splitFile(char** arr, int mode) {
+	int i = 0;
+	char** ptr = NULL;
+	while (i < sizeof(arr)) {
+		int ctr = 0;
+		ptr = (char**)malloc(10*sizeof(char*));
+
+		while ((strcmp(arr[i], ";") != 0) && (strcmp(arr[i], "NULL") != 0)) {
+			ptr[ctr] = arr[i];
+			ctr++;
+			i++;
+		}
+
+//		splitLine(ptr, mode);
+
+		if (strcmp(arr[i], "NULL") == 0) {
+			break;
+		}
+
+		i++;
+	}
+
+	return 1;
+}
+
 int main() {
 	/* Main Function Variables */
 	char *buffer = NULL;
