cf643121a31b149a86642a2cbbb7c50e1716df42
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-17 22:16:00 -0700

    add file.c

--- kylieq-CIS415F19-P1/file.c
+++ kylieq-CIS415F19-P1/file.c
@@ -3,6 +3,47 @@
 #include <stdlib.h>
 #include <string.h>
 
+void copyFile(char *sourcePath, char *destinationPath) { /*for the cp command*/
+	char newSrc[strlen(sourcePath)-1];
+
+	// First check if working directory needs to be changed.
+	// Get rid of unneccesary white-space characters at end of string.
+	if (sourcePath[0] == '.' && sourcePath[1] == '.') {
+		changeDir("..");
+		int i;
+		for (i=0; i<strlen(sourcePath); i++) {
+			newSrc[i] = sourcePath[i];
+		}
+		newSrc[i] = '\0';
+		memmove(newSrc, newSrc+3, strlen(newSrc));
+	}
+	else {
+		int i;
+		for (i=0; i<strlen(sourcePath); i++) {
+			newSrc[i] = sourcePath[i];
+		}
+		newSrc[i] = '\0';
+	}
+
+	FILE *fpSrc = fopen(newSrc, "r");
+	if (fpSrc == NULL) {
+		printf("Error: Source file not found: %s\n", newSrc);
+	}
+	else if (strcmp(destinationPath, ".") == 0) {
+		printf("Error: Destination file name not valid.\n");
+	}
+	else {
+		FILE *fpDst = fopen(destinationPath, "w");
+		char input;
+
+		while ((input = fgetc(fpSrc)) != EOF) {
+			fputc(input, fpDst);
+		}
+		fclose(fpSrc);
+		fclose(fpDst);
+	}
+}
+
 int checkCommand(char *token) {
 	// If command requires 0 args, return 1
 	// If command requires 1 arg, return 2
