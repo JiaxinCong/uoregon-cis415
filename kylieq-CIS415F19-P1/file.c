e7f04d548006f7c0e4ef3e98c854cf93e60d82e3
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-17 22:04:09 -0700

    add file.c

---
+++ kylieq-CIS415F19-P1/file.c
@@ -0,0 +1,48 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	char file_name[] = "input.txt";
+	FILE *fp = fopen(file_name, "r");
+
+	if (fp == NULL) {
+		printf("Error: File not found: %s\n", file_name);
+		exit(1);
+	}
+
+	char *buffer = NULL;
+	size_t bufsize = 100;
+	size_t num_char = 0;
+
+	const char *s = " \n";
+	char *token;
+
+	char **ptr;
+	while ((num_char = getline(&buffer, &bufsize, fp)) != -1) {
+		int ctr = 0;
+		ptr = (char**)malloc(20*sizeof(char*));
+
+		printf("\n%s\n", buffer);
+
+		token = strtok(buffer, s);
+
+		ptr[0] = token;
+		ctr = 1;
+
+        while (token != NULL) {
+           	token = strtok(NULL, s);
+          	ptr[ctr++] = token;
+        }
+
+        ptr[ctr-1] = "NULL";
+//        splitTokens(ptr, -1);
+        free(ptr);
+	}
+
+	/*Free the allocated memory*/
+	free(buffer);
+	fclose(fp);
+	return 1;
+}
