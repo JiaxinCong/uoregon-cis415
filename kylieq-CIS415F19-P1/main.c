eec82e3f68d566fbca29926a1e15857f40087818
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-18 21:00:28 -0700

    Add changes

--- kylieq-CIS415F19-P1/main.c
+++ kylieq-CIS415F19-P1/main.c
@@ -58,11 +58,11 @@ int makeCall(char** arr, int mode) {
 	/* If command is not recoginzed */
 	if (ctr == 1 && check == 0) {
 		if (mode == -1) { printf(">>> %s\n", command); }
+		printf("command: %s check: %d ctr: %d\n", command, check, ctr);
 		printf("Error! Unrecognized command: %s\n", command);
 	}
 	else if (check != ctr) {
 		if (mode == -1) { printf(">>> %s\n", command); }
-		printf("command: %s check: %d ctr: %d\n", command, check, ctr);
 		printf("Error! Unsupported parameters found for command: %s\n", command);
 	}
 	else {
