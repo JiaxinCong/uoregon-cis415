0bd558d66402b7126d636b9917ac86fe9652f93c
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-19 15:59:02 -0700

    add changes

--- kylieq-CIS415F19-P1/main.c
+++ kylieq-CIS415F19-P1/main.c
@@ -2,10 +2,193 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "command.h"
 
-int checkCommand(char *token) {
+/*--------------------------Start of File Mode Functions--------------------------*/
+int checkCommand_File(char *token) {
+    // If command requires 0 args, return 1
+    // If command requires 1 arg, return 2
+    // If commmand requires 2 args, return 3
+    // If command not valid, return 0
+
+    if (strcmp(token, "ls") == 0) { // 0 args
+        return 1;
+    }
+    else if (strcmp(token, "pwd") == 0) { // 0 args
+        return 1;
+    }
+    else if (strcmp(token, "mkdir") == 0) { // 1 arg
+        return 2;
+    }
+    else if (strcmp(token, "cd") == 0) { // 1 arg
+        return 2;
+    }
+    else if (strcmp(token, "cp") == 0) { // 2 args
+        return 3;
+    }
+    else if (strcmp(token, "mv") == 0) { // 2 args
+        return 3;
+    }
+    else if (strcmp(token, "rm") == 0) { // 1 arg
+        return 2;
+    }
+    else if (strcmp(token, "cat") == 0) { // 1 arg
+        return 2;
+    }
+    else {
+        return 0; 
+    }
+}
+
+int print_File(char *command, char **arr, size_t arrSize) {
+
+    if (strcmp(command, "ls") == 0) {
+        printf(">>> %s\n", command); 
+        listDir();
+    }
+    else if (strcmp(command, "pwd") == 0) {
+        printf(">>> %s\n", command); 
+        showCurrentDir();
+    }
+    else if (strcmp(command, "mkdir") == 0) {
+        char *newDir = arr[0];
+        printf(">>> %s %s\n", command, newDir); 
+        makeDir(newDir);
+    }
+    else if (strcmp(command, "cd") == 0) {
+        char *newDir = arr[0];
+        printf(">>> %s %s\n", command, newDir); 
+        changeDir(newDir);
+    }
+    else if (strcmp(command, "cp") == 0) {
+        char *fileSrc = arr[0];
+        char *fileDst = arr[1];
+        printf(">>> %s %s %s\n", command, fileSrc, fileDst); 
+        copyFile(fileSrc, fileDst);
+    }
+    else if (strcmp(command, "mv") == 0) {
+        char *fileSrc = arr[0];
+        char *fileDst = arr[1];
+        printf(">>> %s %s %s\n", command, fileSrc, fileDst); 
+        moveFile(fileSrc, fileDst);
+    }
+    else if (strcmp(command, "rm") == 0) {
+        char *filename = arr[0];
+        printf(">>> %s %s\n", command, filename); 
+        deleteFile(filename);
+    }
+    else if (strcmp(command, "cat") == 0) {
+        char *filename = arr[0];
+        printf(">>> %s %s\n", command, filename); 
+        displayFile(filename);
+    }
+    return 1;
+}
+
+int parseCommand_File(char **arr, size_t arrSize) {
+
+    const char *s = " ";
+    char **args;
+
+    for (int i=0; i<arrSize; i++) {
+
+        if (arr[i] == NULL) {
+            continue;
+        }
+
+        else {
+            int ctr = 0;
+            char *line = arr[i];
+            char *token = strtok(line, s);
+            char *command = token;
+
+            int check = checkCommand_File(command);
+
+            size_t argSize = 2;
+            args = (char**)malloc(argSize * sizeof(char*));
+
+            for (int i=0; i<argSize; i++) {
+                args[i] = NULL;
+            }
+
+            int idx = 0;
+            while (token != NULL) {
+                token = strtok(NULL, s);
+                args[idx] = token;
+                ctr++;
+                idx++;
+            }
+            
+            if (ctr == check) {
+                print_File(command, args, argSize);
+
+            }
+        }
+    }
+    return 1;
+}
+
+int getline_File(char *filename, char *buffer, size_t bufferSize){
+
+    int file = open(filename, O_RDONLY);
+    if (file == -1) {
+        printf("Error: unable to open file '%s'\n", filename);
+        return -1;
+    }
+
+    if (read(file, buffer, bufferSize) == -1) {
+        printf("Error: unable to read file '%s'\n", filename);
+        return -1;
+    }
+
+    close(file);
+
+    return 1;
+}
+
+/* File Mode */
+int filemode(char filename[]) {
+    char *buffer = NULL;
+    size_t bufferSize = 300;
+    ssize_t inputSize = 0;
+
+    buffer = (char *)malloc(bufferSize * sizeof(char));
+    inputSize = getline_File(filename, buffer, bufferSize);
+
+    char *token;
+
+    size_t ptrSize = 300;
+    char **ptr = (char **)malloc(ptrSize*sizeof(char*));
+    for (int i=0; i<ptrSize; i++) {
+        ptr[i] = NULL;
+    }
+
+    token = strtok(buffer, ";\n");
+    ptr[0] = token;
+    int i = 1;
+
+    while (token != NULL) {
+        token = strtok(NULL, ";\n");
+        ptr[i] = token;
+        i++;
+    }   
+
+    printf("\n");
+    parseCommand_File(ptr, ptrSize);
+
+    free(ptr);
+    free(buffer);
+    return 0;
+}
+/*---------------------------End of File Mode Functions---------------------------*/
+
+/*-----------------------Start of Interactive Mode Functions----------------------*/
+int checkCommand_Interactive(char *token) { /* for interactive mode */
 	// If command requires 0 args, return 1
 	// If command requires 1 arg, return 2
 	// If commmand requires 2 args, return 3
@@ -40,7 +223,7 @@ int checkCommand(char *token) {
     }
 }
 
-int makeCall(char** arr, int mode) {
+int makeCall_Interactive(char** arr, int mode) { /* for interactive mode */
 	int ctr = 0;
 	/* Get total count of valid entries */
 	for (int i=0; i<sizeof(arr); i++) {
@@ -53,7 +236,7 @@ int makeCall(char** arr, int mode) {
 	   on the command that is called. If this value does not equal ctr, the 
 	   call is invalid */
 	char *command = arr[0];
-	int check = checkCommand(command);
+	int check = checkCommand_Interactive(command);
 
 	/* If command is not recoginzed */
 	if (ctr == 1 && check == 0) {
@@ -115,7 +298,7 @@ int makeCall(char** arr, int mode) {
 	return 1;
 }
 
-int splitTokens(char** arr, int mode) {
+int splitTokens_Interactive(char** arr, int mode) { /* for interactive mode */
 	int i = 0;
 	char** ptr = NULL;
 	while (i < sizeof(arr)) {
@@ -128,7 +311,7 @@ int splitTokens(char** arr, int mode) {
 			i++;
 		}
 
-		makeCall(ptr, mode);
+		makeCall_Interactive(ptr, mode);
 
 		if (strcmp(arr[i], "NULL") == 0) {
 			break;
@@ -138,48 +321,6 @@ int splitTokens(char** arr, int mode) {
 		free(ptr);
 	}
 	free(ptr);
-	return 1;
-}
-
-/* File Mode */
-int filemode(char file_name[]) {
-	FILE *fp = fopen(file_name, "r");
-
-	if (fp == NULL) {
-		printf("Error: File not found: %s\n", file_name);
-		exit(1);
-	}
-
-	char *buffer = NULL;
-	size_t bufsize = 100;
-	size_t num_char = 0;
-
-	const char *s = " \n";
-	char *token;
-
-	char **ptr;
-	while ((num_char = getline(&buffer, &bufsize, fp)) != -1) {
-		int ctr = 0;
-		ptr = (char**)malloc(20*sizeof(char*));
-
-		token = strtok(buffer, s);
-
-		ptr[0] = token;
-		ctr = 1;
-
-        while (token != NULL) {
-           	token = strtok(NULL, s);
-          	ptr[ctr++] = token;
-        }
-
-        ptr[ctr-1] = "NULL";
-        splitTokens(ptr, -1);
-        free(ptr);
-	}
-
-	/*Free the allocated memory*/
-	free(buffer);
-	fclose(fp);
 	return 1;
 }
 
@@ -228,7 +369,7 @@ int intermode() {
 
 		/* Check if user entered anything on the command line at all */
 		if (strcmp(ptr[0], "NULL") != 0) {
-        	splitTokens(ptr, -2);
+        	splitTokens_Interactive(ptr, -2);
         }
 
 		if (token != NULL) {
@@ -241,6 +382,7 @@ int intermode() {
 	free(buffer);
 	return 1;
 }
+/*------------------------End of Interactive Mode Functions-----------------------*/
 
 int main(int argc, char *argv[]) {
 
