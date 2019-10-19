1d037c6b442148ddab97c338cebbd956a006cb74
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-18 19:20:22 -0700

    Add cat

--- kylieq-CIS415F19-P1/FileTests/cat.c
+++ kylieq-CIS415F19-P1/FileTests/cat.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[]) {
-    FILE *fp = fopen(argv[1], "r");
+/*    FILE *fp = fopen(argv[1], "r");
     if (fp == NULL) {
         printf("File Not Found\n");
     }
@@ -16,4 +17,13 @@ int main(int argc, char *argv[]) {
     }
     fclose(fp);
     free(buffer);
+}*/
+
+    int fd, sz;
+    char *c = (char *)calloc(300, sizeof(char));
+    fd = open("input.txt", O_RDONLY);
+    if (fd < 0) { perror("error\n"); exit(1); }
+    sz = read(fd, c, 300);
+    c[sz] = '\0';
+    printf("%s\n", c);
 }
