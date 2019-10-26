ec07522932ecca85e16a7b9ee30be0481d43795a
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-26 13:21:25 -0700

    Add changes

--- kylieq-CIS415F19-P2/part1.c
+++ kylieq-CIS415F19-P2/part1.c
@@ -48,6 +48,10 @@ int main(int argc, char *argv[]) {
 
 	buffer = (char *)malloc(bufferSize * sizeof(char));
 
+	for (int i=0; i<bufferSize; i++) {
+		buffer[i] = NULL;
+	}
+
 	inputSize = getline_File(filename, buffer, bufferSize);
 
     /* Place '\0' at the end of the string held in the input buffer 
