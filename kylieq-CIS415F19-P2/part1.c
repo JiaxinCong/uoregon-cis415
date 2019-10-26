e5e684912df725212a5601c6cebb4134f47c54c8
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-10-26 12:32:38 -0700

    add changes

--- kylieq-CIS415F19-P2/part1.c
+++ kylieq-CIS415F19-P2/part1.c
@@ -48,7 +48,7 @@ int main(int argc, char *argv[]) {
     /* Place '\0' at the end of the string held in the input buffer 
        to signify the end of the string. */
 	if (inputSize > 0) {
-		buffer[bufferSize] = '\0';
+		buffer[bufferSize-1] = '\0';
 	}
 	else {
 		buffer[0] = '\0';
