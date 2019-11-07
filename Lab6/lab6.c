a01e9eeb75ab87e26fa1b2857a2cf05f3f5cf577
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-11-06 20:33:30 -0800

    add changes

--- Lab6/lab6.c
+++ Lab6/lab6.c
@@ -10,6 +10,7 @@ Author: Jared Hall
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
+#include <time.h>
 /*---------------------------------------------------------------------------*/
 
 /*---------------------------------Functions---------------------------------*/
@@ -22,25 +23,50 @@ int main(void)
     pid_t pid, w;
     int wstatus, eStatus;
 
+    clock_t start;
+    int elapsed_t;
+
     //create a child process
     pid = fork();
     if(pid < 0) {
         perror("Error! could not create a new process.\n");
         exit(EXIT_FAILURE);
-    } else if(pid == 0) {
-        //This code runs in the child process only
-	printf("    Child process: %d - Starting...\n", getpid());
-	//Add your while loop here
-	exit(EXIT_SUCCESS);
+    } 
+    else if(pid == 0) {
+        start = clock();
+        elapsed_t = (double) start/CLOCKS_PER_SEC;
 
-    } else if(pid > 0) {
+        //This code runs in the child process only
+	    printf("    Child process: %d - Starting...\n", getpid());
+	    //Add your while loop here
+        int ctr = 1;
+        while (ctr <= 10) {
+            sleep(1);
+            printf("    Child process: %d - Still alive after for %d seconds...\n", getpid(), ctr);
+            ctr++;
+        }
+	    exit(EXIT_SUCCESS);
+    } 
+    else if(pid > 0) {
         //This code runs in the parent process only
-	printf("Parent process: %d - Sending signals to child...\n", getpid());
-	//Add code to send your signals  in a loop here
-	
-	printf("Parent process: %d - Waiting for child to complete...\n", getpid());
-	w = waitpid(pid, &wstatus, 0);
-	printf("Parent process: %d - Finished\n", getpid());
+        //sleep(1);
+        printf("Parent process: %d - Sending signals to child...\n", getpid());
+	    //Add code to send your signals  in a loop here
+        printf("Parent process: %d - Waiting for child to complete...\n", getpid());
+
+        w = waitpid(pid, &wstatus, WNOHANG);
+        while (w == 0) {
+            printf("    Child process: %d - Stopped...\n", pid);
+            kill(pid, SIGSTOP);
+            sleep(3);
+            printf("    Child process: %d - Continued...\n", pid);
+            kill(pid, SIGCONT);
+            sleep(1);
+            w = waitpid(pid, &wstatus, WNOHANG);
+
+        }
+
+	    printf("Parent process: %d - Finished\n", getpid());
     }
     
     //exit out of program
