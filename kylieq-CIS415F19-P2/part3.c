83f8f979b5ee7b59ab2183c08e34e943ec93a26d
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-11-09 13:38:01 -0800

    Add changes

--- kylieq-CIS415F19-P2/part3.c
+++ kylieq-CIS415F19-P2/part3.c
@@ -71,7 +71,7 @@ void SuspendAllProcesses(struct ProcessControlBlock **PCBS) {
         if (kill(PCBS[i]->pid, SIGSTOP) == 0) {
             printf("Process: %d - Suspended\n", PCBS[i]->pid);
             PCBS[i]->STATE = NOTSTARTED;
-            printf("STATE: %d\n", PCBS[i]->STATE);
+            printf("STATE: %d\n", PCBS[i]->STATE==NOTSTARTED);
             sleep(1);
         }
     }
@@ -83,7 +83,7 @@ void ContinueAllProcesses(struct ProcessControlBlock **PCBS) {
         if (kill(PCBS[i]->pid, SIGCONT) == 0) {
             printf("Process: %d - Continued\n", PCBS[i]->pid);
             PCBS[i]->STATE = RUNNING;
-            printf("STATE: %d\n", PCBS[i]->STATE);
+            printf("STATE: %d\n", PCBS[i]->STATE==RUNNING);
             sleep(1);
         }
     }
@@ -95,7 +95,7 @@ int TerminateAllProcesses(struct ProcessControlBlock **PCBS) {
         wait(NULL);
         printf("Process: %d - Ended\n", PCBS[i]->pid);
         PCBS[i]->STATE = TERMINATED;
-        printf("STATE: %d\n", PCBS[i]->STATE);
+        printf("STATE: %d\n", PCBS[i]->STATE==TERIMATED);
         sleep(1);
     } 
     return 1;
