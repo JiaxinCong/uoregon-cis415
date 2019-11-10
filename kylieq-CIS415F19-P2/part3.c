bf038b216158cc7b9a4a6f7a9033f4fd1976881f
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-11-09 17:41:57 -0800

    Add changes

--- kylieq-CIS415F19-P2/part3.c
+++ kylieq-CIS415F19-P2/part3.c
@@ -67,6 +67,7 @@ void sigalrm_handler(int sig_num) {
     }
 
     printf("Process: %d Counter: %d\n", PCBS[COUNTER]->pid, COUNTER);
+    printf("check: %d\n", PCBS[COUNTER]->STATE == RUNNING);
     while(1) {
         if (PCBS[COUNTER]->STATE == RUNNING) {
             kill(PCBS[COUNTER]->pid, SIGSTOP);
