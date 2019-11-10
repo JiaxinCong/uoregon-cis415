74ba732a8384a43c104159d87dc6e70cc2c41cd8
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-11-10 10:26:43 -0800

    Add changes

--- kylieq-CIS415F19-P2/part3.c
+++ kylieq-CIS415F19-P2/part3.c
@@ -93,7 +93,10 @@ void sigalrm_handler(int sig_num) {
         printf("Stuck in while for SIGCONT\n");
         if (PCBS[COUNTER]->STATE == STOPPED && PCBS[COUNTER]->exit_status != 1) {
             printf("PROPER STATES TO CONTINUE\n");
-            if (kill(PCBS[COUNTER]->pid, SIGCONT) == 0) {
+            int result = kill(PCBS[COUNTER]->pid, SIGCONT);
+            printf("result of kill(): %d", result);
+            //if (kill(PCBS[COUNTER]->pid, SIGCONT) == 0) {
+            if (result == 0) {
                 printf("Process: %d - Received Signal SIGALRM - Continued\n", PCBS[COUNTER]->pid);
                 PCBS[COUNTER]->STATE = RUNNING;
             }
