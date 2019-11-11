9a88d13885a91d74c4c2ab2fbff34db228a16f14
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-11-10 23:52:18 -0800

    Add changes

--- kylieq-CIS415F19-P2/part3.c
+++ kylieq-CIS415F19-P2/part3.c
@@ -41,13 +41,13 @@ void SigUsr1Handler(int sig_num) {
 
 void SigChldHandler(int sig_num) {
     int status;
-        if (waitpid(PCBS[COUNTER]->pid, &status, WNOHANG) != 0) {
+        if (waitpid(PCBS[COUNTER]->pid, &status, WNOHANG) > 0) {
             if (WIFEXITED(status)) {
                 printf("Process: %d - Terminated\n", PCBS[COUNTER]->pid);
                 PCBS[COUNTER]->exit_status = 1;
             }
             else
-                printf("status: %d\n", status);
+                PCBS[COUNTER]->exit_status = 1;
         }
 }
 
