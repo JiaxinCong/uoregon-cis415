31b479a6ec8975090b28c070bbb962c48a800ff8
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-12-02 23:52:09 -0800

    Add changes

--- kylieq-CIS415F19-P3/bounded_queue.c
+++ kylieq-CIS415F19-P3/bounded_queue.c
@@ -110,7 +110,7 @@ int BB_IsEmpty(struct bounded_queue *queue) {
 
 void BB_FreeBoundedQueue(struct bounded_queue *queue) {
 	for (int i=0; i<queue->size; i++) {
-        queue->buffer[i] = NULL;
+        free(queue->buffer[i]);
     }
 	free(queue->buffer);
 	free(queue);
