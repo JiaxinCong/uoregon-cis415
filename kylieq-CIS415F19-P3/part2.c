5a59de91d7b43951a5f43158a81cf73a4701ef8d
Kylie Quan <Kylie@Kylies-MacBook-Air-6.local>
2019-12-03 19:05:44 -0800

    Add changes

--- kylieq-CIS415F19-P3/part2.c
+++ kylieq-CIS415F19-P3/part2.c
@@ -23,25 +23,15 @@ void *Publisher(void *arg) {
 	return 0;
 }
 
-void *Cleanup(void *arg) {
-	pthread_mutex_lock(&lock);
-	pthread_cond_wait(&cond, &lock); // block until condition var cond is broadcast to
-	printf("Proxy thread %u - type: %s\n", (unsigned int)pthread_self(), (char*)arg);
-	pthread_mutex_unlock(&lock);
-	return 0;
-}
-
 int main(int argc, char *argv[]) {
-	pthread_t subscriber, publisher, cleanup;
+	pthread_t subscriber, publisher;
 
 	char *arg1 = "Subscriber";
 	char *arg2 = "Publisher";
-	char *arg3 = "Cleanup";
 
 	// Create threads
 	int result1 = pthread_create(&subscriber, NULL, Subscriber, arg1);
 	int result2 = pthread_create(&publisher, NULL, Publisher, arg2);	
-	int result3 = pthread_create(&cleanup, NULL, Cleanup, arg3);
 
 	sleep(1);
 
@@ -51,9 +41,8 @@ int main(int argc, char *argv[]) {
 	// Wait for threads to terminate
 	int check1 = pthread_join(subscriber, NULL);
 	int check2 = pthread_join(publisher, NULL);
-	int check3 = pthread_join(cleanup, NULL);
 
-	if (check1 == 0 && check2 == 0 && check3 == 0) {
+	if (check1 == 0 && check2 == 0) {
 		printf("All threads have successfully terminated.\n");
 	}
 
